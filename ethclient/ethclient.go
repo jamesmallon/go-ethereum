80b71c744b27f0394963feb99714d5598e812e70
Jordan Schalm <jordan.schalm@gmail.com>
2017-12-15 14:34:10 -0800

    Add txpool_content method to builtin Go RPC client

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -476,6 +476,15 @@ func (ec *Client) SendTransaction(ctx context.Context, tx *types.Transaction) er
 	return ec.c.CallContext(ctx, nil, "eth_sendRawTransaction", common.ToHex(data))
 }
 
+// TxPoolContent returns the current contents of the pending pool.
+// NOTE: Unmarshalling the result into type rpcTransaction loses all
+// transaction data except (blockNumber, blockHash, from).
+func (ec *Client) TxPoolContent(ctx context.Context) (map[string]map[string]map[string]*types.Transaction, error) {
+	var res map[string]map[string]map[string]*types.Transaction
+	err := ec.c.CallContext(ctx, &res, "txpool_content")
+	return res, err
+}
+
 func toCallArg(msg ethereum.CallMsg) interface{} {
 	arg := map[string]interface{}{
 		"from": msg.From,
